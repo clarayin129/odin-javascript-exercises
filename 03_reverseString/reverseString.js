12c79d0ec0afc4b4b295a836312685379d208b48
clarayin129 <clarayin129@gmail.com>
2025-02-10 22:36:18 -0800

    Exercises 1-3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array = string.split("");
+    let returnString = "";
+    for (let i = array.length - 1; i >= 0; i--) {
+        returnString += array[i];
+    }
+    return returnString;
 };
 
 // Do not edit below this line
