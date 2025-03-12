43ae2346835b5d65be27c5c31dcd28e9a3f655a7
clarayin129 <clarayin129@gmail.com>
2025-03-12 11:33:19 -0700

    exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const sum = arr.reduce((total, curr) => total += curr, 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  const mult = arr.reduce((total, curr) => total *= curr, 1);
+  return mult;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let res = a;
+	for (let i = 0; i < b - 1; i++) {
+    res *= a;
+  }
+  return res;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a == 0) return 1;
+	for (let i = a - 1; i > 0; i--) {
+    a *= i;
+  }
+  return a;
 };
 
 // Do not edit below this line
