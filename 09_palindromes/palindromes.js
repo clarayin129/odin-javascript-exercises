913663a4f478fb5ec4cc9dfb567f79fe41ef10b7
clarayin129 <clarayin129@gmail.com>
2025-03-13 21:14:38 -0700

    exercises 9-12

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
 const palindromes = function (str) {
+  const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+  const cleanedString = string
+    .toLowerCase()
+    // split into array of characters
+    .split('')
+    // keep valid ones
+    .filter((character) => alphanumerical.includes(character))
+    .join('');
+
+  const reversedString = cleanedString.split('').reverse().join('');
+
+  return cleanedString === reversedString;
 };
 
 // Do not edit below this line
